54b4dbb0923563118d5310b29d6e5d0a6d4278fd
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-01-02 13:38:42 +0530

    Yml file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/[SocialMedia-App]/",
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
